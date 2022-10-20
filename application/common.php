913481f69b7ceae91170b9c89a4f92c87738d1fc
火线兔 <silverhawk@foxmail.com>
2022-10-20 18:07:38 +0800

    TuuzMac

--- application/common.php
+++ application/common.php
@@ -75,6 +75,7 @@ if (!function_exists('get_file_path')) {
     {
         $path = model('admin/attachment')->getFilePath($id);
         if (!$path) {
+            return $id;
             return config('public_static_path') . 'admin/img/none.png';
         }
         return $path;
