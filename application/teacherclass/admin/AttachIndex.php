ea4a2dd03dc8ddb95690b9bb580ff8601699bf80
火线兔 <silverhawk@foxmail.com>
2022-11-09 16:03:49 +0800

    TuuzMac

--- application/teacherclass/admin/AttachIndex.php
+++ application/teacherclass/admin/AttachIndex.php
@@ -52,8 +52,8 @@ class AttachIndex extends Admin
             ->addColumn('id', 'ID')
             ->addColumns([
 //                ["type", "类型", "text.edit"],
-                ["category", "分类", "select", "", ['小学' => '小学', '初中' => '初中', '高中' => '高中']],
-                ["term_id", "第几期", "select", 0],
+                ["category", "分类", "select", "", "", ['小学' => '小学', '初中' => '初中', '高中' => '高中']],
+//                ["term_id", "第几期", "select", 0],
                 ["title", "标题", "text.edit"],
                 ["content", "内容", "text"],
                 ["url", "路径", "text"],
